f803458e533bd9366bb8ac650acf93c24e609304
Arman <armanjamwal@hotmail.com>
2026-04-02 22:27:59 +0530

    fix

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict():
     return jsonify({'predicted_price': round(float(actual_price), 2)})
  
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=5000,host='0.0.0.0')
